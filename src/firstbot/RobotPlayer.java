a7a69721be5ed3f320cee76adcc92313096fa88a
Tyler Feldman <tylerfeldman321@gmail.com>
2021-01-06 16:21:10 -0800

    Create first bot

---
+++ src/firstbot/RobotPlayer.java
@@ -0,0 +1,80 @@
+package firstbot;
+import battlecode.common.*;
+
+public strictfp class RobotPlayer {
+    static RobotController rc;
+
+    static final RobotType[] spawnableRobot = {
+            RobotType.POLITICIAN,
+            RobotType.SLANDERER,
+            RobotType.MUCKRAKER,
+    };
+
+    static final Direction[] directions = {
+            Direction.NORTH,
+            Direction.NORTHEAST,
+            Direction.EAST,
+            Direction.SOUTHEAST,
+            Direction.SOUTH,
+            Direction.SOUTHWEST,
+            Direction.WEST,
+            Direction.NORTHWEST,
+    };
+
+    static int turnCount;
+
+    public static void run(RobotController rc) throws GameActionException {
+
+        RobotPlayer.rc = rc;
+        turnCount = 0;
+
+        System.out.println("I'm a " + rc.getType() + " and I just got created!");
+        while (true) {
+            turnCount += 1;
+            // Try/catch blocks stop unhandled exceptions, which cause your robot to freeze
+            try {
+                // Here, we've separated the controls into a different method for each RobotType.
+                // You may rewrite this into your own control structure if you wish.
+                System.out.println("I'm a " + rc.getType() + "! Location " + rc.getLocation());
+                switch (rc.getType()) {
+                    case ENLIGHTENMENT_CENTER: runEnlightenmentCenter(); break;
+                    case POLITICIAN:           runPolitician();          break;
+                    case SLANDERER:            runSlanderer();           break;
+                    case MUCKRAKER:            runMuckraker();           break;
+                }
+
+                // Clock.yield() makes the robot wait until the next turn, then it will perform this loop again
+                Clock.yield();
+
+            } catch (Exception e) {
+                System.out.println(rc.getType() + " Exception");
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void runEnlightenmentCenter() {
+        if (turnCount == 0) {
+            findMapBorders();
+        }
+    }
+
+    public static void runSlanderer() {
+
+    }
+
+    public static void runPolitician() {
+
+    }
+
+    public static void runMuckraker() {
+
+    }
+
+    public static void findMapBorders() {
+        MapLocation currentLocation = rc.getLocation();
+
+
+    }
+
+}
